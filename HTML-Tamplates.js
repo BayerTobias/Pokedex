e0f6ac35b68e8d7e79f18ca454937ac297e6214f
BayerTobias <tbayer2@gmx.de>
2023-05-30 21:06:51 +0200

    many features added

--- HTML-Tamplates.js
+++ HTML-Tamplates.js
@@ -1,8 +1,34 @@
-function returnHTML(name, imgUrl, id) {
+function getMiniCard(name, imgUrl, id, type) {
   return /*html*/ `
-  <div onclick="showOverview(${id})" class="mini-card">
-    <div><b>${name}<b></div>
+  <div onclick="showOverview(${id})" class="mini-card ${type}">
+    <div><b>${name}</b></div>
+    <div>#${id}</div>
     <img src="${imgUrl}" alt="">
   </div>
   `;
+}
+
+function getDetailCard(pokemon) {
+  return /*html*/ `
+  <div id="card-header">${pokemon.name}</div>
+      <div>
+        <div id="card-types"></div>
+        <img id="card-img" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="pokemon-img" />
+      </div>
+      <div class="lower-card">
+        <div><a href="">Stats</a><a href="">Moves</a></div>
+        <div id="info-box"></div>        
+`;
+}
+
+function getBar(statName, statValue) {
+  return /*html*/ `
+  <div><span> ${statName}</span>
+  <div class="myProgress">
+    <div id="${statName}-bar" class="myBar" style="width: ${getPercent(
+    statValue
+  )}%">${statValue}</div>
+  </div>
+  </div>
+`;
 }
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let loadedPokemon = 0;
 let loadPokemon = 30;
-let lastExecution = 0;
+let results;
+let infiniteScrollActive = "active";
 
 async function getData() {
-  let url = `https://pokeapi.co/api/v2/pokemon?limit=${loadPokemon}&offset=${loadedPokemon}`;
-  let response = await fetch(url);
-  let responseAsJson = await response.json();
-  let results = responseAsJson.results;
-  console.log("Pokemon Data", results);
-  renderPokemon(results);
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+  const response = await fetch(url);
+  const responseAsJson = await response.json();
+  results = responseAsJson.results;
+  renderPokemon();
 }
 
-async function renderPokemon(results) {
-  let main = document.getElementById("main");
-  for (let i = 0; i < results.length; i++) {
+async function renderPokemon() {
+  const main = document.getElementById("main");
+  for (let i = loadedPokemon; i < loadPokemon; i++) {
     const pokemon = results[i];
     loadedPokemon++;
     main.innerHTML += /*html*/ `
@@ -22,32 +22,108 @@ async function renderPokemon(results) {
   }
 }
 
+async function loadMorePokemon() {
+  loadPokemon = loadPokemon + 30;
+  await renderPokemon();
+}
+
 async function getPokemonData(url) {
-  let response = await fetch(url);
-  let responseAsJson = await response.json();
-  let imgUrl = responseAsJson.sprites.other["official-artwork"].front_default;
-  let name = responseAsJson.name;
-  let id = responseAsJson.id;
-  return returnHTML(name, imgUrl, id);
+  const response = await fetch(url);
+  const responseAsJson = await response.json();
+  const imgUrl = responseAsJson.sprites.other["official-artwork"].front_default;
+  const name = responseAsJson.name;
+  const id = responseAsJson.id;
+  const type = responseAsJson.types[0].type.name;
+  return getMiniCard(name, imgUrl, id, type);
+}
+
+async function searchPokemon() {
+  const searchInput = document
+    .getElementById("search-input")
+    .value.toLowerCase();
+  infiniteScrollActive = "notactive";
+  main.innerHTML = ``;
+  for (let i = 0; i < results.length; i++) {
+    const pokemon = results[i];
+    const pokemonName = pokemon.name;
+    if (pokemonName.includes(searchInput)) {
+      await renderSearchedPokemon(pokemon);
+    }
+  }
+}
+
+async function renderSearchedPokemon(pokemon) {
+  const main = document.getElementById("main");
+  main.innerHTML += await getPokemonData(pokemon.url);
 }
 
 async function showOverview(id) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
   const pokemon = await response.json();
+  const overlayBg = document.getElementById("overlay-bg");
+  overlayBg.classList.remove("d-none");
+  fillCard(pokemon);
+  getTypes(pokemon);
+  renderBars(pokemon);
   console.log(pokemon);
 }
 
-function infiniteScroll() {
+function fillCard(pokemon) {
+  const container = document.getElementById("detailes-card");
+  container.innerHTML = getDetailCard(pokemon);
+}
+
+function renderBars(pokemon) {
+  const barContainer = document.getElementById("info-box");
+
+  for (let i = 0; i < pokemon.stats.length; i++) {
+    const statName = pokemon.stats[i].stat.name;
+    const statValue = pokemon.stats[i].base_stat;
+    barContainer.innerHTML += getBar(statName, statValue);
+  }
+}
+
+function getPercent(statValue) {
+  const maxStat = 200;
+  const percent = (statValue / maxStat) * 100;
+  return percent;
+}
+
+function getTypes(pokemon) {
+  const types = pokemon.types;
+  const typeBox = document.getElementById("card-types");
+
+  for (let i = 0; i < types.length; i++) {
+    const type = types[i];
+    typeName = type.type.name;
+    let typediv = document.createElement("div");
+    typediv.textContent = typeName;
+    typeBox.appendChild(typediv);
+  }
+}
+
+function closeOverlay() {
+  const overlayBg = document.getElementById("overlay-bg");
+  overlayBg.classList.add("d-none");
+}
+
+function doNotClose(event) {
+  event.stopPropagation();
+}
+
+function reloadPage() {
+  location.reload();
+}
+
+async function infiniteScroll() {
   if (
     window.scrollY + window.innerHeight + 50 >=
-    document.documentElement.scrollHeight
+      document.documentElement.scrollHeight &&
+    infiniteScrollActive == "active"
   ) {
-    const delay = 2000; //anti-rebound 2second
-
-    if (lastExecution + delay < Date.now()) {
-      getData();
-      lastExecution = Date.now();
-    }
+    infiniteScrollActive = "notactive";
+    await loadMorePokemon();
+    infiniteScrollActive = "active";
   }
 }
 
